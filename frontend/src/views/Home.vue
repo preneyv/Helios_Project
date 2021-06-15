a5f6fa02ec63c23c83bb126c589d68bc5004ed65
clairedornic <50169132+clairedornic@users.noreply.github.com>
2021-06-15 12:51:44 +0200

    Home page done + setup scss

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,114 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <div class="logo-container">
+
+      <img :src="require('@/assets/logo.png')" alt="logo Helios">
+    </div>
+    <div class="content-container">
+       <h1>Échangez anonymement avec des amoureux de belles voitures !</h1>
+        <p>Participez à des événements, partagez vos photos et échangez avec d'autres passionnés en toute tranquilité</p>
+        <div class="login">
+          <h2>Rejoignez la communauté !</h2>
+          <div class="cta-login">
+            <router-link to="/">S'inscrire</router-link>
+            <router-link to="/">Se connecter</router-link>
+          </div>
+        </div> 
+    </div>
   </div>
 </template>
 
+<style lang="scss">
+#app {
+  font-family: Poppins, Helvetica, Arial, sans-serif;
+}
+
+.home {
+  display: inline-flex;
+  height: 100vh;
+  width: 100vw;
+  background-image: url("../assets/bg-home.jpg");
+  background-size: 100%;
+  background-repeat: no-repeat;
+  color: $white;
+
+  .logo-container {
+    width: 45%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  img {
+    height: 310px;
+    width: 310px;
+  }
+
+  .content-container {
+    width: 55%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 0 5%;
+  }
+
+  .login {
+      width: 90%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+
+      .cta-login {
+        display: inline-flex;
+        justify-content: space-around;
+        width: 90%;
+      }
+    }
+
+  h1 {
+    font-size: 35px;
+    font-weight: 600;
+    line-height: 52px;
+  }
+
+  p {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 24px;
+  }
+
+  h2 {
+    font-size: 25px;
+    font-weight: 600;
+    padding: 50px 0 25px 0;
+  }
+
+  .cta-login {
+    a {
+      &:first-of-type {
+        @include btnBlue;
+      }
+
+      &:last-of-type {
+        @include btnBorderWhite;
+      }
+    }
+  }
+}
+
+</style>
+
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>

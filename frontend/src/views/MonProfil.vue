9aa38a35cbff1fc91771a3f7d3844aeb84d276d0
audreylbf <audrey.le.bouffant.lbf@gmail.com>
2021-06-17 14:45:34 +0200

    profil categ

---
+++ frontend/src/views/MonProfil.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="monProfil">
+    <div class="titrePage">Mon profil</div>
+    
+
+  </div>
+</template>
+
+<style lang="scss">
+  .monProfil {
+    padding-left:23%;
+    padding-right:24%;
+    padding-top:4%;
+    color:white;
+  }
+
+  .titrePage {
+    color:white;
+    font-size:135%;
+    font-weight: bold;
+    padding-bottom:3%;
+  }
+</style>
+
+

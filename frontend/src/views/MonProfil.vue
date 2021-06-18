9d63e370fafeb539353457fb6a78a6bc475d5ec7
audreylbf <audrey.le.bouffant.lbf@gmail.com>
2021-06-18 10:50:53 +0200

    update profil

--- frontend/src/views/MonProfil.vue
+++ frontend/src/views/MonProfil.vue
@@ -1,14 +1,48 @@
-<template>
-<div class="main">
-  <div class="monProfil">
-    <div class="h1">Mon profil</div>
+<template> 
+  <div class="main">
+      <div class="monProfil">
+          <div class="titrePage">Mon profil</div>
+      </div>
   </div>
-</div>
 </template>
+<script>
+    /* eslint-disable */ 
+    import Nav from '../components/Nav.vue'
+    import BandeauRight from '../components/BandeauRight.vue'
+    import ProfilGroupes from '../components/ProfilGroupes.vue'
+    import ProfilInfos from '../components/ProfilInfos.vue'
+    import ProfilPosts from '../components/ProfilPosts.vue'
+    // import AuthServices from "@/services/auth.js"
+
+    export default {
+        name: 'monProfil',
+        components: {
+            Nav,
+            BandeauRight,
+            ProfilGroupes,
+            ProfilInfos,
+            ProfilPosts
+        },
+
+    }
+</script>
 
 <style lang="scss">
 
+.monProfil {
+    padding-left:23%;
+    padding-right:24%;
+    padding-top:4%;
+    color:white;
+  }
+
+  .titrePage {
+    color:white;
+    font-size:135%;
+    font-weight: bold;
+    padding-bottom:3%;
+  }
+
 
 </style>
-
 
